709f411351465af42e384fdec7229769af3d6ed0
athulsreekumar <athult2000@gmail.com>
2024-08-19 12:50:35 +0530

    added base strucuture

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.scss';
+import Main
+  from './components/main/main';
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/main/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './main.scss'
+import './../../fonts/Reiswar.ttf'
+export default function Main() {
+  
+  return (
+    <div className="main">
+
+      <div className="toppart">
+        athul.sreekumar
+      </div>
+      <div className="mainContent">
+        <div className="card" id='bio'>
+          <div className="cardContent">
+          👋 Hey there! I'm Athul, your friendly neighborhood coder 🧙‍♂️<br />
+          Imagine a world where websites are as smooth as butter and deploy themselves. That's the world I'm creating, one line of code at a time! <br />
+          🎨Whether I'm designing sleek user interfaces or automating deployments, I bring a dash of creativity to everything I touch. My secret sauce? A perfect blend of frontend finesse and backend brilliance. <br />
+          🌟 When I'm not coding, you'll find me designing graphics, creating content, or dreaming up the next big app idea.
+          </div>
+        </div>
+        <div className="card">
+
+          <div className="cardTitle">
+            <p>Projects</p>
+          </div>
+          
+        </div>
+        <div className="card">
+        <div className="cardTitle">
+        <p>UI Design Library
+          </p>
+          </div>
+          
+        </div>
+        <div className="card">
+        <div className="cardTitle">
+        <p>Blogs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
